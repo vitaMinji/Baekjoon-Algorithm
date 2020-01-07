102803e2db4ba7353d904933b047d2e6e1554430
vitaMinji <46252678+vitaMinji@users.noreply.github.com>
2020-01-07 17:25:38 +0900

    [10171]-c++

---
+++ [10171]-c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	cout<<"\\    /\\ "<<endl;
+	cout<<" )  ( ')"<<endl;
+	cout<<"(  /  )"<<endl;
+	cout<<" \\(__)|"<<endl;
+	
+}
+
+//백슬래쉬 하나는 출력 안됨
+ 
